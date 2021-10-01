9671ef5e5b04fcee957a7549fcfea956c7322a06
kamlendu <kamlendup@gmail.com>
2021-10-01 12:31:28 +0530

    MS 01-10--2021

--- shopmicroprofile-jwt-docker/kubernetes/removeservice.sh
+++ shopmicroprofile-jwt-docker/kubernetes/removeservice.sh
@@ -1,11 +1,12 @@
 ufw disable
 kubectl delete -f https://k8s.io/examples/admin/dns/dnsutils.yaml
-kubectl remove secret generic mysql-pass --from-literal=password=ompandey
+#kubectl remove secret generic mysql-pass --from-literal=password=ompandey
 kubectl delete -f mysql-pv.yaml
 kubectl delete -f mysql-service.yaml
 kubectl delete -f stocknew-service.yaml
 kubectl delete -f productnew-service.yaml
 kubectl delete -f shopfrontnew-service.yaml
+kubectl delete -f jpapayara-service.yaml
 #kubectl delete -f ingress-service.yaml
 #minikube addons enable ingress
 
